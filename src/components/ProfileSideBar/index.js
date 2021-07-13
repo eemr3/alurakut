963e0003e1e4e278f26b143c0f5960ac072a353e
Emerson <eemr3@yahoo.com.br>
2021-07-13 15:40:17 -0300

    refector: create componet ProfileSideBar

---
+++ src/components/ProfileSideBar/index.js
@@ -0,0 +1,26 @@
+import Box from "../Box";
+import { AlurakutProfileSidebarMenuDefault } from "../../lib/AlurakutCommons";
+function ProfileSideBar({ githubUser }) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${githubUser}.png`}
+        style={{ borderRadius: "8px" }}
+      />
+      <hr />
+      <p>
+        <a
+          className="boxLink"
+          href={`https://github.com/${githubUser}`}
+          target="_blank"
+        >
+          @{githubUser}
+        </a>
+      </p>
+      <hr />
+      <AlurakutProfileSidebarMenuDefault />
+    </Box>
+  );
+}
+
+export default ProfileSideBar;
