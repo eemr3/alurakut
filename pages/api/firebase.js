029aebf4d6a029f5910b59b02a2c5355c1d280f5
Emerson Moreira <eemr3@yahoo.com.br>
2021-07-16 16:26:16 -0300

    feat: install firebase/auth

---
+++ pages/api/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+};
+
+if (!firebase.apps.length) {
+  Firebase.initializeApp(firebaseConfig);
+}
+export default firebase;
